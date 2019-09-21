9ecac4a5d2b40046738d44c71202ff66d9a65c3a
shunpei <shunpei42ba@gmail.com>
2019-09-21 12:21:30 +0900

    adjust position of button

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,10 +2,10 @@ namespace Position {
 
   export const dome = new Vector3(25, 20, 7.5)
   export const staircase = new Vector3(-10, 0, 7.5)
-  export const landing = new Vector3(31, 10, 7.5)
+  export const landing = new Vector3(31, 9, 7.5)
 
   export const defaultPhoton = new Vector3(0, 0, 0)
-  export const bluePhotonBase = new Vector3(40, 12, 6)
-  export const redPhotonBase = new Vector3(40, 12, 7.5)
-  export const greenPhotonBase = new Vector3(40, 12, 9)
+  export const bluePhotonBase = new Vector3(30, 16, 4)
+  export const redPhotonBase = new Vector3(35, 16, 7.5)
+  export const greenPhotonBase = new Vector3(30, 16, 11)
 }
