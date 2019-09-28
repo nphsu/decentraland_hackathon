47c421206b57c7cce9fa7779004b6970ddf33fd1
shunpei <shunpei42ba@gmail.com>
2019-09-28 14:34:57 +0900

    add trigger open gate

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,6 +14,34 @@ namespace Position {
   export const countdownBoard = new Vector3(40, 18, 7.5)
   // export const scoreBoard = new Vector3(40, )
 
+  export const initBox01 = new Vector3(-8, 3, 5)
+  export const initBox02 = new Vector3(-8, 3, 11)
+  export const initBox03 = new Vector3(-8, 9, 5)
+  export const initBox04 = new Vector3(-8, 9, 11)
+  export const initBox05 = new Vector3(-2, 9, 5)
+  export const initBox06 = new Vector3(-2, 9, 11)
+  export const initBox07 = new Vector3(-2, 3, 5)
+  export const initBox08 = new Vector3(-2, 3, 11)
+  export const initBox09 = new Vector3(4, 3, 5)
+  export const initBox10 = new Vector3(4, 3, 11)
+  export const initBox11 = new Vector3(4, 9, 5)
+  export const initBox12 = new Vector3(4, 9, 11)
+  export const initBox13 = new Vector3(4, 9, -1)
+
+  export const initBox51 = new Vector3(4, 16, 4.5)
+  export const initBox52 = new Vector3(4, 16, 11.5)
+  export const initBox53 = new Vector3(4, 16, 4.5)
+  export const initBox54 = new Vector3(4, 16, 11.5)
+  export const initBox55 = new Vector3(4, 16, 4.5)
+  export const initBox56 = new Vector3(-2, 9, 11.5)
+  export const initBox57 = new Vector3(-2, 3, 4.5)
+  export const initBox58 = new Vector3(-2, 3, 11.5)
+  export const initBox59 = new Vector3(4, 3, 4.5)
+  export const initBox60 = new Vector3(4, 3, 11.5)
+  export const initBox61 = new Vector3(4, 9, 11.5)
+  export const initBox62 = new Vector3(4, 9, 11.5)
+  export const initBox63 = new Vector3(4, 3, 11.5)
+
   export const box01 = new Vector3(7, 3, 0)
   export const box02 = new Vector3(13, 3, -6)
   export const box03 = new Vector3(7, 9, -6)
@@ -28,7 +56,6 @@ namespace Position {
   export const box12 = new Vector3(55, 3, 0)
   export const box13 = new Vector3(-5, 3, 0)
   
-  
   export const box51 = new Vector3(7, 3, 15)
   export const box52 = new Vector3(13, 3, 21)
   export const box53 = new Vector3(7, 9, 21)
@@ -42,7 +69,6 @@ namespace Position {
   export const box61 = new Vector3(49, 9, 21)
   export const box62 = new Vector3(55, 3, 15)
   export const box63 = new Vector3(-5, 3, 15)
-  // export const box06 = new Vector3(31, 3, -12)
   
 }
 
